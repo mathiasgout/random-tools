f367f8c8b73158bfb312955d6b732691d5dc9687
Mathias Gout <mathias.gout@gmail.com>
2023-06-25 22:07:25 +0200

    FileHandler -> WatchedFileHandler

--- random_tools/logger/logger_tools.py
+++ random_tools/logger/logger_tools.py
@@ -1,18 +1,22 @@
-import logging
 import os
 import time
+import logging
+from logging.handlers import WatchedFileHandler
 from pathlib import Path
-
-from typing import Union
+from typing import Optional
 
 
 def get_loggers(
-    logger_names: list[str], stream: bool = True, file_path: Union[str, None] = None
+    logger_names: list[str],
+    level: str = "DEBUG",
+    stream: bool = True,
+    file_path: Optional[str] = None,
 ) -> list[logging.Logger]:
     """Create logger(s)
 
     Args:
         logger_names (list[str]): list of logger names
+        level (str): loggers level. Defaults to DEBUG
         stream (bool, optional): to add a StreamHandler. Defaults to True.
         file_path (Union[str, None], optional): path of file for the FileHandler. Defaults to None.
 
@@ -23,7 +27,7 @@ def get_loggers(
     loggers = []
     for logger_name in logger_names:
         logger = logging.getLogger(logger_name)
-        logger.setLevel(logging.DEBUG)  # Modification du niveau de criticité du logger
+        logger.setLevel(level)  # Modification du niveau de criticité du logger
         loggers.append(logger)
 
     # Création d'un formatteur
@@ -51,7 +55,7 @@ def get_loggers(
         FOLDER_PATH = os.path.dirname(os.path.realpath(file_path))
         Path(FOLDER_PATH).mkdir(parents=True, exist_ok=True)
 
-        file_handler = logging.FileHandler(file_path)
+        file_handler = WatchedFileHandler(file_path)
         file_handler.setFormatter(formatter)  # Liaison le formatteur au handler
         for logger in loggers:
             logger.addHandler(file_handler)
--- tests/logger/test_logger_tools.py
+++ tests/logger/test_logger_tools.py
@@ -5,5 +5,5 @@ import logging
 
 def test_get_logger_TYPE():
     loggers = logger_tools.get_loggers(logger_names=["logger_name"])
-    assert type(loggers) == list    
+    assert type(loggers) == list
     assert type(loggers[0]) == logging.Logger
