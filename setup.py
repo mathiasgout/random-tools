77d12ac706f97bfe2d22c96b65f097139b84255e
Mathias Gout <mathias.gout@gmail.com>
2023-06-26 22:54:35 +0200

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="random-tools",
-    version="0.0.4",
+    version="0.0.5",
     author="Mathias Gout",
     packages=find_packages(exclude=["tests"]),
     python_requires="==3.9.*",
